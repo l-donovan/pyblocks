3a458cef8b1cb1f433c0881e6976bfbd46fd7540
Luke Donovan <ladbuilder1@gmail.com>
2020-04-01 19:39:53 -0400

    initial commit

---
+++ blocks/base.py
@@ -0,0 +1,180 @@
+class Block():
+    def __init__(self):
+        pass
+
+class Program():
+    def __init__(self, root_block):
+        self.root_block = [root_block] if (type(root_block) not in [tuple, list]) else root_block
+
+    def run(self):
+        scope = {}
+        for item in self.root_block:
+            scope, result = item.run(scope)
+
+class ForLoop(Block):
+    def __init__(self, iterator, iterable, body):
+        self.iterator = iterator
+        self.iterable = iterable
+        self.body = [body] if (type(body) not in [tuple, list]) else body
+
+    def run(self, scope={}):
+        for i in self.iterable:
+            scope[self.iterator] = i
+            for item in self.body:
+                scope, result = item.run(scope)
+        return scope, None
+
+class WhileLoop(Block):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = [body] if (type(body) not in [tuple, list]) else body
+
+    def run(self, scope={}):
+        while (True):
+            scope, result = self.condition.run(scope)
+            if (not result):
+                break
+            for item in self.body:
+                scope, result = item.run(scope)
+        return scope, None
+
+class Set(Block):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def run(self, scope={}):
+        scope, literal = self.value.run(scope)
+        scope[self.name] = literal
+        return scope, None
+
+class Get(Block):
+    def __init__(self, name):
+        self.name = name
+
+    def run(self, scope={}):
+        return scope, scope[self.name]
+
+class Print(Block):
+    def __init__(self, literal):
+        self.literal = literal
+
+    def run(self, scope={}):
+        scope, value = self.literal.run(scope)
+
+        if (type(value) is Literal):
+            scope, value = value.run(scope)
+
+        print(value, end='')
+
+        return scope, None
+
+class If(Block):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = [body] if (type(body) not in [tuple, list]) else body
+
+    def run(self, scope={}):
+        scope, outcome = self.condition.run(scope)
+        if (outcome):
+            for item in self.body:
+                scope, result = item.run(scope)
+        return scope, outcome
+
+class Else(Block):
+    def __init__(self, body):
+        self.body = [body] if (type(body) not in [tuple, list]) else body
+
+    def run(self, scope={}):
+        for item in self.body:
+            item.run(scope)
+        return scope, True
+
+class ConditionSection(Block):
+    def __init__(self, conditions):
+        self.conditions = conditions
+
+    def run(self, scope={}):
+        for condition in self.conditions:
+            outcome = condition.run(scope)
+            if (outcome):
+                break
+        return scope, None
+
+class Literal(Block):
+    def __init__(self, value):
+        self.value = value
+
+    def run(self, scope={}):
+        return scope, self.value
+
+class Math(Block):
+    def __init__(self, operator, args):
+        self.operator = operator
+        self.args = args
+
+    def run(self, scope={}):
+        new_args = []
+
+        for arg in self.args:
+            scope, result = arg.run(scope)
+            if (type(result) is Literal):
+                scope, result = result.run(scope)
+            new_args.append(result)
+
+        if (self.operator == '+'):
+            out = Literal(sum(new_args))
+        elif (self.operator == '-'):
+            out = Literal(new_args[0] + sum([-arg for arg in new_args[1:]]))
+        elif (self.operator == '*'):
+            out = Literal(math.prod(new_args))
+        elif (self.operator == '/'):
+            out = Literal(new_args[0] / new_args[1])
+        else:
+            out = None
+
+        return scope, out
+
+class Return(Block):
+    def __init__(self, item):
+        self.item = item
+
+    def run(self, scope={}):
+        scope, result = self.item.run(scope)
+        return scope, result
+
+class FunctionHelper():
+    def __init__(self, arg_names, body):
+        self.arg_names = arg_names
+        self.body = body
+
+    def call(self, scope={}):
+        for item in self.body:
+            scope, result = item.run(scope)
+        return scope, result
+
+class Function(Block):
+    def __init__(self, name, arg_names, body):
+        self.name = name
+        self.arg_names = arg_names
+        self.body = [body] if (type(body) not in [tuple, list]) else body
+
+    def run(self, scope={}):
+        scope[self.name] = FunctionHelper(self.arg_names, self.body)
+        return scope, None
+
+class Call(Block):
+    def __init__(self, fn_name, args):
+        self.fn_name = fn_name
+        self.args = [args] if (type(args) not in [tuple, list]) else args
+
+    def run(self, scope={}):
+        fn = scope[self.fn_name]
+        fn_scope = scope
+
+        for i, v in enumerate(fn.arg_names):
+            fn_scope[v] = self.args[i]
+
+        fn_scope, result = fn.call(fn_scope)
+
+        return scope, result
---
+++ demo.py
@@ -0,0 +1,42 @@
+from blocks.base import *
+
+def main():
+    print('Program 1')
+    Program(
+        ForLoop('i', range(10), [
+            Print(Literal('i is: ')),
+            Print(Get('i')),
+            Print(Literal('\n'))
+        ])
+    ).run()
+
+    print('Program 2')
+    Program([
+        Print(Math('+', [Literal(2), Literal(3)])),
+        Print(Literal('\n'))
+    ]).run()
+
+    print('Program 3')
+    Program([
+        Set('i', Literal(0)),
+        Print(Literal('i is: ')),
+        Print(Get('i')),
+        Print(Literal('\n')),
+        Set('i', Math('+', [Get('i'), Literal(1)])),
+        Print(Literal('i is: ')),
+        Print(Get('i')),
+        Print(Literal('\n'))
+    ]).run()
+
+    print('Program 4')
+    Program([
+        Function('test', 'n', [
+            Set('n', Math('+', [Get('n'), Literal(10)])),
+            Return(Get('n'))
+        ]),
+        Print(Call('test', 3)),
+        Print(Literal('\n'))
+    ]).run()
+
+if (__name__ == '__main__'):
+    main()
